a599af87114c80e397e62d109308d48576536e38
Schnabl,Oliver <Oliver.Schnabl@drei.com>
2021-12-08 09:03:57 +0100

    Initial Maven Setup

---
+++ perfserver/src/main/java/net/inemar/perfserver/App.java
@@ -0,0 +1,13 @@
+package net.inemar.perfserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
