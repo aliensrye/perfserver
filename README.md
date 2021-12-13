308b1ef9f74ed3fee28f8989f3689b513da5d5c3
Schnabl,Oliver <Oliver.Schnabl@drei.com>
2021-12-13 13:45:47 +0100

    Initial Version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # perfserver
 This is a simple JSON Server, that delivers pseudo performance data. It used to test programming capabilities
+
+1) The Simulator
+The first thing is a simulator, that simulate providing Performance data.
+
+It can be startet with java -jar perfserver.jar or is found as the responsible maven project.
+This launches a webserver with the folliwng functions on http://127.0.0.1:8001
+
+/hello  -> This answers with Hello World
+/getdata ->  This shall be called with the following parameter ?time=[unix timestamp in seconds]
+
+http://127.0.0.1:8001/getdata?time=1639062504
+
+This delivers a CSV file in the folling format
+
+Timestamp|Duration|Key|[Name of values|]*
+
+
+
+
+2) Requirements for the program.
+
+The programm shall retrieve for a given 5 minute slice the performance data from the server.
+The unix timestamp shall be the timestamp at the beginning of the time interval.
+If the start time of the program is smaller than current time - 5 minutes another time slice shall be retrieved.
+After successful retrieving a time slice the start date of the timeslice shall be written to a file on the disk.
+
+If the program is started again the file shall be read and retrieving of data shall start with the next time slice.
+The date/time in the file shall be in a human understandable format.
+
+The output shall be one or more valid JSON files with the following format
+
+- each line is a valid JSON format
+- {starttime:"|ISO 8601 Format|",duration:|duration in minutes|,key:{key:"|Value of Key|"},value:{|Name-Val1|:|val1|,|Name-Val2|:|val2|,...}}
+
+
--- perfserver/src/main/java/net/inemar/perfserver/App.java
+++ perfserver/src/main/java/net/inemar/perfserver/App.java
@@ -1,13 +1,132 @@
 package net.inemar.perfserver;
 
+import java.util.Random;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import com.sun.net.httpserver.*;
+
+
+
+ class Resp {
+    static   void sendResponse200(HttpExchange httpExchange,String data) throws IOException {
+
+        httpExchange.sendResponseHeaders(200,0);
+        OutputStream outputStream = httpExchange.getResponseBody();
+                
+                outputStream.write(data.getBytes());
+                outputStream.flush();
+                outputStream.close();
+        
+    
+    
+    }
+
+    static   void sendError(HttpExchange httpExchange,int error,String data) throws IOException {
+
+        httpExchange.sendResponseHeaders(error,0);
+        OutputStream outputStream = httpExchange.getResponseBody();
+                
+                outputStream.write(data.getBytes());
+                outputStream.flush();
+                outputStream.close();
+        
+    
+    
+    }
+
+    
+    static Random random=new Random(System.currentTimeMillis());
+
+
+    static String[] vals= new String[] { "CPU","Power","attempts","rulesmatched","Germany","hungary","Linkload","memory","CPU1","CPU2","CPU3","CPU4","CPU5","CPU6","CPU7","CPU0"};
+
+}
+
+
+final class MyHelloHandler implements HttpHandler {    
+      @Override    
+      public void handle(HttpExchange httpExchange) throws IOException {
+         System.out.println("Got "+httpExchange.getRequestMethod()); 
+        String method=httpExchange.getRequestMethod();
+        if("GET".equals(method)) { 
+           Resp.sendResponse200(httpExchange, "Hello World!");
+         }else  { 
+             Resp.sendError(httpExchange, 400, "Only GET Method is allowed- Got: "+method);
+          }  
+      }
+    }
+
+
+
+final class GetDataHandler implements HttpHandler {    
+        @Override    
+        public void handle(HttpExchange httpExchange) throws IOException {
+          String method=httpExchange.getRequestMethod();
+          String query=httpExchange.getRequestURI().getQuery();
+          if("GET".equals(method)) { 
+              if (query.startsWith("time=")) {
+                  long time=Long.parseLong(query.substring(5).toString());
+                  if (time % (5*60) != 0 ) {
+                    Resp.sendError(httpExchange, 400, "time is not at 5 minutes interval  time= "+time);
+                  };
+                  time=time*1000;
+                  if (time > System.currentTimeMillis()-(5*60*1000) ) {
+                    Resp.sendError(httpExchange, 400, "time is in the future   time= "+time);
+                  };
+
+                  if (time < System.currentTimeMillis()-(3*24*60*60*1000) ) {
+                    Resp.sendError(httpExchange, 400, "time is too much in the past more than 3 days  time= "+time);
+                  };
+
+
+                  int val=Math.abs(Resp.random.nextInt(Resp.vals.length-5))+5;
+                  String resp="Timestamp|Duration|Key";                  
+                  for(int i=0;i<val;i++) {
+                      resp=resp+"|"+Resp.vals[i];
+                  };
+                  resp=resp+"|\n";
+                  int lines=Math.abs(Resp.random.nextInt(40))+10;
+                    for (int i=0;i<lines;i++) {
+                      resp=resp+""+(time*1000)+"|5|mykey"+Resp.random.nextInt(30000);
+
+                      for(int j=0;j<val;j++) {
+                        resp=resp+"|"+Resp.random.nextDouble();                          
+                      }
+                      resp=resp+"\n";
+                    }
+                    Resp.sendResponse200(httpExchange,resp);
+
+              };
+              Resp.sendError(httpExchange, 400, "the time paramter is missing or wrong  query= "+query);
+           }else  { 
+               Resp.sendError(httpExchange, 400, "Only GET Method is allowed- Got: "+method);
+            }  
+        }
+      }
+  
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException 
     {
-        System.out.println( "Hello World!" );
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+
+
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
+        server.createContext("/hello", new  MyHelloHandler());
+        server.createContext("/getdata", new  GetDataHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+
+        System.out.println(" Server started on port 8001");
+        
     }
 }
